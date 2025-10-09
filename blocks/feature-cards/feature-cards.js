2a37aa67a5a75e96ac16cfd372988da73c8eca02
Shivam Tanwar <shivam.tanwar@axeno.co>
2025-10-09 16:56:55 +0530

    qwertyuio

--- blocks/feature-cards/feature-cards.js
+++
@@ -1,6 +0,0 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
-export default function decorate(block) {
-  console.log(...block.children);
-}
